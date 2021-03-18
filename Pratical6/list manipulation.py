a137d7e53edc58504f11e42ecf821f94bb284844
Shenghan426 <shenghan.20@intl.zju.edu.cn>
2021-03-19 00:20:27 +0800

    python

---
+++ Pratical6/list manipulation.py
@@ -0,0 +1,14 @@
+gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,15981]# the list of
+exon_counts=[51,1142,42,216,25,650,32533,57,1,523]
+average_exon_lengths=[a/b for a,b in zip(gene_lengths,exon_counts)]
+print(average_exon_lengths)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=[184.50980392156862, 345.1322241681261, 105.76190476190476, 487.6759259259259, 765.96, 118.12153846153846, 3.8898964128730826, 636.7719298245614, 842.0, 30.55640535372849]
+
+df = pd.DataFrame(data)
+df.plot.box(title='average gene length')
+plt.grid(linestyle="--", alpha=0.3)
+plt.show()
